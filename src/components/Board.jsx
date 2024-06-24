461f6635855632e19e3751e7da97c5cd66f8f4df
tjsm <mathincosmos@gmail.com>
2024-06-24 09:13:41 +0530

    make the box clickable

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,16 +1,45 @@
+import { useState } from "react";
+
 const Board = () => {
-  const boxes = Array.from({ length: 50 * 50 }, (_, i) => (
-    <div
-      key={i}
-      className="box w-2 h-2 md:w-[9px] md:h-[9px] border border-gray-600"
-    ></div>
-  ));
+  const [gameState, setGameState] = useState(0); // 0 means game is stop right now
+  const [matrix, setMatrix] = useState(
+    Array.from({ length: 50 }, (_, rowIndex) =>
+      Array.from({ length: 50 }, (_, colIndex) => ({
+        value: 0,
+        rowIndex,
+        colIndex,
+        key: `${rowIndex}-${colIndex}`,
+      }))
+    )
+  );
+
+  const handleClick = (i, j) => {
+    const newMatrix = matrix.map((row, rowIndex) =>
+      row.map((v, colIndex) => {
+        if (i == rowIndex && colIndex == j) {
+          return { ...v, value: !matrix[i][j].value };
+        }
+        return v;
+      })
+    );
+    setMatrix(newMatrix);
+  };
 
   return (
     <div className="md:flex-1 mt-4 items-center md:flex flex-col gap-2">
       <Action />
       <div className="grid my-8 w-max border-gray-600 border-4 border-double grid-cols-[repeat(50,minmax(0,1fr))]">
-        {boxes}
+        {matrix.map((arr) =>
+          arr.map((v) => (
+            <Box
+              handleClick={handleClick}
+              rowIndex={v.rowIndex}
+              colIndex={v.colIndex}
+              value={v.value}
+              key={v.key}
+            />
+          ))
+        )}
       </div>
       <p>Generation Count: 12</p>
     </div>
@@ -26,10 +55,20 @@ const Action = () => {
         <option>Glider Gun</option>
         <option>Pulsar</option>
         <option>Maximum Density Still Life</option>
-        <option>Glider Gun</option>
       </select>
       <button className="bg-mygreen p-1  active:bg-[#6bcb47]">Clear</button>
     </div>
+  );
+};
+
+const Box = ({ handleClick, value, rowIndex, colIndex }) => {
+  return (
+    <div
+      className={`box w-2 h-2 md:w-[9px] md:h-[9px] ${
+        value ? "bg-mygreen" : ""
+      } border border-gray-600`}
+      onClick={() => handleClick(rowIndex, colIndex)}
+    ></div>
   );
 };
 
